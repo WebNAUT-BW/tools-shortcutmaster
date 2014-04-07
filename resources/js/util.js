8020124e0fa6e79d1171332c389ac92e03e2f23e
Kotaro Shimoda <k_shimoda@beeworks.co.jp>
2014-04-07 14:50:36 +0900

    ・キーボードタイプを選択してスタイルを変更可能に ・英語用キーが不足していたのでHTMLに追加 ・マウスオーバー時のボタンスタイル切り替えように#keyboard-wrapというdiv追加

--- resources/js/util.js
+++ resources/js/util.js
@@ -1,15 +1,37 @@
+/* ===============================================
+# Table Mouseover
+=============================================== */
 $(function () {
 	$('.ovTable tbody tr').on('mouseenter' ,function(event) {
 		event.preventDefault();
 		/* Act on the event */
 		//console.log('hover');
 		var _this = $(this);
-		var _getClass = $(this).attr('class');
+		var _tableClass = $(this).attr('class');
+
+		$('#keyboard-wrap').attr('class','');
+		$('#keyboard-wrap').addClass(_tableClass);
 		console.log(_getClass);
 
-
-
-
-
+	});
+	$('.ovTable tbody tr').on('mouseout' ,function(event) {
+		event.preventDefault();
+		$('#keyboard-wrap').attr('class','');
 	});
 });
+
+/* ===============================================
+# keyboardType Select
+=============================================== */
+$(function () {
+	$( 'input[name="keyboardType"]:radio' ).change( function() {
+		var _val = $( this ).val();
+		var _keyboardBase = $('#keyboard-base');
+		_keyboardBase.attr('class','');
+		_keyboardBase.addClass(_val);
+	});
+});
+
+/* ===============================================
+# Local Storage
+=============================================== */
