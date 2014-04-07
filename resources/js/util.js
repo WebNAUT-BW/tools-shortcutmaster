6bc7028489af6a632d761d992779c891aca32cbb
Kotaro Shimoda <k_shimoda@beeworks.co.jp>
2014-04-07 20:03:46 +0900

    ・キーボード切り替え状態をLocalStrageに記録する機能追加 ・チェックボックスの値をLocalStrageに記録する機能追加

--- resources/js/util.js
+++ resources/js/util.js
@@ -11,7 +11,7 @@ $(function () {
 
 		$('#keyboard-wrap').attr('class','');
 		$('#keyboard-wrap').addClass(_tableClass);
-		console.log(_getClass);
+		//console.log(_getClass);
 
 	});
 	$('.ovTable tbody tr').on('mouseout' ,function(event) {
@@ -23,15 +23,63 @@ $(function () {
 /* ===============================================
 # keyboardType Select
 =============================================== */
+//ラジオボタン選択時に選択状態を記録
 $(function () {
 	$( 'input[name="keyboardType"]:radio' ).change( function() {
 		var _val = $( this ).val();
 		var _keyboardBase = $('#keyboard-base');
 		_keyboardBase.attr('class','');
 		_keyboardBase.addClass(_val);
+		window.localStorage.setItem("keyboardType",_val);
 	});
 });
 
+//ページロード時にキーボード選択状態を呼び出し、チェックを入れる
+$(document).ready(function() {
+	var data = window.localStorage.getItem("keyboardType");
+	var _input = $('input[name="keyboardType"]');
+	if (data == "") {
+		return;
+	} else {
+		console.log('data=' + data);
+		$("input[value='" + data  + "']:radio").attr('checked', 'checked');
+		var _keyboardBase = $('#keyboard-base');
+		_keyboardBase.attr('class','');
+		_keyboardBase.addClass(data);
+		return;
+	}
+});
+
 /* ===============================================
-# Local Storage
+# Like Select
 =============================================== */
+//クリック時にチェックボックスの値を記録
+$(function () {
+	$( 'input:checkbox' ).change( function() {
+		var _val = $( this ).is(':checked');
+		var _id = $( this ).parents('tr').attr('id');
+		console.log('_val=' + _val);
+		console.log('_id=' + _id);
+		window.localStorage.setItem(_id,_val);
+		// var _keyboardBase = $('#keyboard-base');
+		// _keyboardBase.attr('class','');
+		// _keyboardBase.addClass(_val);
+		// window.localStorage.setItem("keyboardType",_val);
+	});
+});
+
+//ページロード時にチェックボックスの値を呼び出し、チェックを入れる
+$(document).ready(function() {
+	$('td.like').each(function() {
+		var _id = $( this ).parents('tr').attr('id');
+		console.log('_id=' + _id);
+		var getItem = window.localStorage.getItem(_id);
+		console.log('getItem=' + getItem);
+		if (getItem == 'true'){
+			console.log('_id=' + _id + ' is true');
+			$(this).find('input:checkbox').attr("checked", true);
+		}
+	});
+});
+
+
